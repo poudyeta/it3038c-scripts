475c88976e84eb551eb61c3791960a96af8b2b34
poudyeta <poudyeta@mail.uc.edu>
2023-09-10 15:54:49 -0400

    Lab 2

---
+++ bash/covid.sh
@@ -0,0 +1,10 @@
+s script downloads covid data and displays it
+DATA=$(curl https://api.covidtracking.com/v1/us/current.json)
+POSITIVE=$(echo $DATA | jq '.[0].positive')
+NEGATIVE=$(echo $DATA | jq '.[0].negative')
+PENDING=$(echo $DATA | jq '.[0].pending')
+DEATHINCREASE=$(echo $DATA | jq '.[0].deathIncrease')
+
+TODAY=$(date)
+
+echo "On $TODAY, there were $POSITIVE positive COVID cases and $NEGATIVE negative cases. The death toll increased by $DEATHINCREASE and there remain $PENDING tests pending"

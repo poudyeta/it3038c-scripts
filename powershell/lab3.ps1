f4687a1e60f5a90f511cd178b72120699e45bcc9
poudyeta <143351948+poudyeta@users.noreply.github.com>
2023-09-17 22:13:41 -0400

    Added ps script for lab 3

---
+++ powershell/lab3.ps1
@@ -0,0 +1,13 @@
+﻿function getIP{
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+
+$IP = getIP
+$User = $env:USERNAME
+$Ver = $HOST.Version.Major
+$Hostname = $env:COMPUTERNAME
+$date = Get-Date -Format "dddd, MMMM dd, yyyy"
+ 
+$BODY = "This machine's IP is $IP. User is $User. Hostname is $Hostname. PowerShell $Ver. Today's Date is $Date"
+
+$BODY | Out-File -FilePath "C:\system_info.txt"
